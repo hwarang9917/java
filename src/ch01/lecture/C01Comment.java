47796b2b13e9da7df6908284325bcac6006df669
hwarang9917 <hwarang9917@gmail.com>
2025-04-28 12:12:28 +0900

    first commit

---
+++ src/ch01/lecture/C01Comment.java
@@ -0,0 +1,15 @@
+package ch01.lecture;
+
+public class C01Comment {
+    public static void main(String[] args) {
+// 한 줄 주석
+/*
+여러 줄 주석
+여러 줄 주석
+ */
+// ctrl /
+//        System.out.println("hi");
+        System.out.println("java");
+        System.out.println("자바");
+    }
+}
